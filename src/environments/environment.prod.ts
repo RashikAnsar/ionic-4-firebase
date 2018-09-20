5130ed5fa069b01432687ac55f9fb3b22d03a253
RashikAnsar <sk.ansar46@gmail.com>
2018-09-20 11:05:32 +0530

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA4fog2u3jTytIY7mTWaB1E-c-Vdpb9E8A',
+    authDomain: 'ion4-fire.firebaseapp.com',
+    databaseURL: 'https://ion4-fire.firebaseio.com',
+    projectId: 'ion4-fire',
+    storageBucket: 'ion4-fire.appspot.com',
+    messagingSenderId: '173787679491'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA4fog2u3jTytIY7mTWaB1E-c-Vdpb9E8A',
+    authDomain: 'ion4-fire.firebaseapp.com',
+    databaseURL: 'https://ion4-fire.firebaseio.com',
+    projectId: 'ion4-fire',
+    storageBucket: 'ion4-fire.appspot.com',
+    messagingSenderId: '173787679491'
+  }
 };
 
 /*
